7b3d9dbeaaba76f55c3e9ce00a81fa94b502e615
zhw <eternalzhw@gmail.com>
2022-06-16 14:37:36 +0800

    feat: 🎸 init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/fogcloud-io/fn-amqp-to-kafka
+
+go 1.17
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.8.2
+	github.com/fogcloud-io/fog-amqp-consumer v0.1.1
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+)
---
+++ handler.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"sync/atomic"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	consumer "github.com/fogcloud-io/fog-amqp-consumer"
+)
+
+var (
+	AMQP_HOST         = os.Getenv("AMQP_HOST")
+	AMQP_PORT         = os.Getenv("AMQP_PORT")
+	FOG_ACCESS_KEY    = os.Getenv("FOG_ACCESS_KEY")
+	FOG_ACCESS_SECRET = os.Getenv("FOG_ACCESS_SECRET")
+)
+
+var (
+	KAFKA_HOST  = os.Getenv("KAFKA_HOST")
+	KAFKA_TOPIC = os.Getenv("KAFKA_TOPIC")
+
+	producer *kafka.Producer
+	msgCnt   int64
+)
+
+func init() {
+	amqpCli := consumer.InitAMQPConsumer(context.Background(), AMQP_HOST, AMQP_PORT, FOG_ACCESS_KEY, FOG_ACCESS_SECRET)
+	producer = initKafkaProducer()
+	go amqpCli.ConsumeWithHanlder(context.Background(), saveMsgToKafka)
+}
+
+// fission function entrypoint
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func initKafkaProducer() (p *kafka.Producer) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": KAFKA_HOST,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func saveMsgToKafka(msg []byte) {
+	producer.Produce(
+		&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &KAFKA_TOPIC, Partition: kafka.PartitionAny},
+			Value:          msg,
+		},
+		nil,
+	)
+	atomic.AddInt64(&msgCnt, 1)
+	if msgCnt%10 == 0 {
+		producer.Flush(10 * 1000)
+	}
+}
